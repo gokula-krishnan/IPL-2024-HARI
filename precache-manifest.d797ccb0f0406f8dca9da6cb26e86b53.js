73d7b18a41c15cc66d722c1674fbc9d50272bdd3
GK <gokula_krishnan@trimble.com>
2024-05-07 23:26:13 +0530

    Updates

---
+++ precache-manifest.d797ccb0f0406f8dca9da6cb26e86b53.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3014258b8527b3a8450e",
+    "url": "/IPL-2024-HARI/static/css/main.eca6c321.chunk.css"
+  },
+  {
+    "revision": "3014258b8527b3a8450e",
+    "url": "/IPL-2024-HARI/static/js/main.3014258b.chunk.js"
+  },
+  {
+    "revision": "e6194d6ef6cdc19d802c",
+    "url": "/IPL-2024-HARI/static/js/runtime~main.e6194d6e.js"
+  },
+  {
+    "revision": "dfd2890e9d31265d1dcb",
+    "url": "/IPL-2024-HARI/static/css/2.b93a0912.chunk.css"
+  },
+  {
+    "revision": "dfd2890e9d31265d1dcb",
+    "url": "/IPL-2024-HARI/static/js/2.dfd2890e.chunk.js"
+  },
+  {
+    "revision": "6e330a93e7a782716c9f087709086766",
+    "url": "/IPL-2024-HARI/index.html"
+  }
+];
